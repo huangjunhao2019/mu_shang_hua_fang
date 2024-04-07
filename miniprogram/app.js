e937f24e5542b74b387f1d8f71dc050ba136febe
huangjunhao <huang.jun.hao@foxmail.com>
2024-04-07 16:46:34 +0800

    104 模态对话框封装

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 // import { toast } from './utils/extendApi'
 import './utils/extendApi'
 App({
-    onShow(){
+    async onShow(){
         // wx.showToast({
         //     // title: "消息提示框",    //提示的内容
         //     // icon: "success",    //提示的图标：success(成功)、
@@ -18,5 +18,21 @@ App({
         // toast({ title: '数据加载完毕', icon: 'success'})
         wx.toast({title: "数据加载完毕", icon: "success"})
         
+        console.log('------')
+        // wx.showModal({
+        //     title: "提示", //提示的标题
+        //     content: "您确定执行该操作吗？",//提示的内容
+        //     confirmColor: '#f3514f',
+        //     //接口调用结束的回调函数（调用成功、失败都会执行）
+        //     complete({confirm, cancel}) {
+        //         confirm && console.log('点击了确定')
+        //         cancel && console.log('点击了取消')
+        //     }
+        // })
+        const res = await wx.modal({
+            title: '新的提示',
+            showCancel: false
+        })
+        console.log(res)
     }
 })
--- miniprogram/utils/extendApi.js
+++ miniprogram/utils/extendApi.js
@@ -12,10 +12,40 @@ const toast = ({ title = '数据加载中...', icon = 'none', duration = 2000, m
       mask
     })
 }
+
+//在调用modal方法时，可以传递参数，也可以不传递参数
+//如果不传递参数，默认值就是空对象
+//如果传递参数，参数需要是一个对象，对象中的属性需要和wx.showModal参数保持一致
+const modal = (options = {}) => {
+    //在方法内部需要通过Promise返回用户的操作
+    //如果用户点击了确定，需要通过resolve返回true
+    //如果用户点击了取消，需要通过resolve返回false
+    return new Promise((resolve)=>{
+        //默认的参数
+        const defaultOpt = {
+            title: '提示',
+            content: '您确定执行该操作吗？',
+            confirmColor: '#f3514f'
+
+        }
+    //通过Object.assign方法将参数进行合并
+        const opts = Object.assign({}, defaultOpt, options)
+        wx.showModal({
+            //将合并以后的参数通过展开运算符赋值给wx.showModal对象
+          ...opts, 
+          complete(confirm, cancel){
+            confirm && resolve(true)
+            cancel && resolve(false)
+          }
+        })
+    })
+}
+
 //如果有很多.js文件，都需要调用toast方法，每次使用点后需要导入toast，然后进行调用，太麻烦
 //可以将toast方法挂载到wx全局对象身上
 wx.toast = toast 
+wx.modal = modal 
 
 //如果其他.js文件，需要使用toast方法
 //需要先导入toast，然后进行调用才可以
-export {toast}
+export {toast, modal}
